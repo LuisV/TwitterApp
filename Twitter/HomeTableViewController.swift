760866a62b3c8953ef813eede80320b1a88f11ca
Luis Valencia <luisvalencia@Luiss-MacBook-Pro.local>
2019-02-25 10:34:55 -0800

    Finished twitter App

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -20,7 +20,7 @@ class HomeTableViewController: UITableViewController {
     
     @objc func loadTweets(){
         let myURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 10]
+        let myParams = ["count": 20]
         TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams, success:
             { (tweets: [NSDictionary]) in
             self.tweetArray.removeAll()
@@ -46,6 +46,13 @@ class HomeTableViewController: UITableViewController {
         loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated);
+        self.loadTweets();
     }
     // https://api.twitter.com/1.1/statuses/home_timeline.json
     // MARK: - Table view data source
@@ -69,6 +76,9 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetID = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted( tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -13,16 +13,63 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     
+    @IBOutlet weak var favButton: UIButton!
+    @IBOutlet weak var reTweetButton: UIButton!
     @IBOutlet weak var contentLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Iitialization code
     }
-
+    var favorited:Bool = false
+    var tweetID:Int = -1
+    //var retweeted:Bool = false
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        } else {
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    @IBAction func favTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if(toBeFavorited){
+            TwitterAPICaller.client?.favTweet(tweetId: tweetID, success: {
+                self.setFavorite(true)
+            }, failure: { (Error) in
+                print("Could not like \(Error)");
+            })
+        } else {
+            TwitterAPICaller.client?.unfavTweet(tweetId: tweetID, success: {
+            self.setFavorite(false)
+            }, failure: { (Error) in
+                 print("Could not unlike \(Error)");
+            })
+        }
+    }
+    
+    @IBAction func reTweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetID, success: {
+            self.setRetweeted(true)
+        }, failure: { (Error) in
+            print("Could not retweet \(Error)");
+        })
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
+    func setRetweeted(_ isRetweeted:Bool){
+        if(isRetweeted){
+            reTweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            reTweetButton.isEnabled = false;
+        } else {
+            reTweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            reTweetButton.isEnabled = true;
+        }
+    }
 }
